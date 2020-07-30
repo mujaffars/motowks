029c0c9b0ae60a8265eff55bab4762251723171a
Mujaffarkhan Sanadi <mujaffarkhan.sanadi@clariontechnologies.co.in>
2020-07-30 19:29:03 +0530

    asdfasdfsadf

--- js/showhide.js
+++ js/showhide.js
@@ -1,55 +1,78 @@
-var tokenPresent = false;
+var tokenPresent=false;
 
 $(window).on('load', function () {
 
-	$('#header #icon_menu, #sidebar-left #icon_menu_close').click(function(){
-		showHideSidebar();
-	})
+    $('#header #icon_menu, #sidebar-left #icon_menu_close').click(function () {
+        showHideSidebar();
+    })
 })
 
-function hideScreen(){
-    
+function hideScreen() {
+
 }
 
-function showLogin(){
-	
+function showLogin() {
+
 }
 
-function showHideSidebar(){
-	if($('#sidebar-left').css('display') !== 'block'){
-		$('#sidebar-left').show("slide", { direction: "left" }, 100);
-	}else{
-		$('#sidebar-left').hide("slide", { direction: "left" }, 100);
-	}
+function showHideSidebar() {
+    if ($('#sidebar-left').css('display')!=='block') {
+        $('#sidebar-left').show("slide", {direction: "left"}, 100);
+    } else {
+        $('#sidebar-left').hide("slide", {direction: "left"}, 100);
+    }
 }
 
-function showThePage(thePage, theFolder){
-	
-	$('#main').html('');
-	
-	$.ajax({
-		url: theFolder+"/"+thePage+'.html',
-		type: 'GET',
-		dataType: 'html',
-		async: true,
-		error: function () {
-		},
-		success: function (resp) {
-			$("#main").html(resp);
-			definePageEvents(thePage, theFolder);	
-		}
-	});
-	
+function showThePage(thePage, theFolder) {
+
+    console.log('Inside show page '+thePage' '+theFolder);
+    $('#main').html('');
+
+    $.ajax({
+        url: theFolder+"/"+thePage+'.html',
+        type: 'GET',
+        dataType: 'html',
+        async: true,
+        error: function () {
+        },
+        success: function (resp) {
+            $("#main").html(resp);
+            definePageEvents(thePage, theFolder);
+        }
+    });
+
 }
 
-function definePageEvents(thePage, theFolder){
-	if(thePage==='add'&&theFolder==='pages/customer'){
-		$("#main").find('#btnCreateCustomer').click(function(){
-			if($("#main").find('#vehicleNo').val()===''){
-				toastr.error('Vehicle No required');
-			}else if($("#main").find('#vehicleNo').val()===''){
-				toastr.error('Vehicle No required');
-			}
-		})
-	}
+function definePageEvents(thePage, theFolder) {
+    if (thePage==='add'&&theFolder==='pages/customer') {
+        $("#main").find('#btnCreateCustomer').click(function () {
+            if ($("#main").find('#vehicleNo').val()==='') {
+                toastr.error('Vehicle No required');
+            } else if ($("#main").find('#vehicleNo').val()==='') {
+                toastr.error('Vehicle No required');
+            }
+
+            var fdata = {
+                'vehicleno': $("#main").find('#vehicleNo').val(),
+                'customername': $("#main").find('#customerName').val(),
+                'mobileno': $("#main").find('#mobileNo').val()
+            };
+            
+            $.ajax({
+                url: theFolder+"/saveCustomer",
+                type: 'POST',
+                data: fdata,
+                dataType: 'json',
+                async: true,
+                error: function () {
+                },
+                success: function (resp) {
+                    if(resp.error){
+                        toastr.error(resp.errormsg);
+                    }
+                }
+            });
+            
+        })
+    }
 }
